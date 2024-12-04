b36287a19c155bb315d3ba9ba205a3cfdd0d6f29
XiangHerry <xiang.che@northeastern.edu>
2024-12-04 12:02:18 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,7 @@ const allowedOrigins = [
     'http://localhost:3000' 
   ]; 
   app.use(cors({
-    origin: function(origin, callback){
-        // Allow requests with no origin (like Postman)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
+    origin: allowedOrigins
 }));
 
 // Use JSON middleware
